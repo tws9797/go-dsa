0b0ae6b4d3cf3fc2733d613b5f74f6bca133d7db
Tee Wen Seng <wensengtee@gmail.com>
2022-09-05 13:00:49 +0800

    move-zeroes

---
+++ arrays/move-zeroes.go
@@ -0,0 +1,30 @@
+package arrays
+
+// https://leetcode.com/problems/move-zeroes/
+func moveZeroes(nums []int) {
+
+	for i, count := 0, 0; i < len(nums); i++ {
+
+		// Use count as pointer to the non-zeros
+		if nums[i] != 0 {
+			nums[i], nums[count] = 0, nums[i]
+			count++
+		}
+	}
+}
+
+func moveZeroes2(nums []int) {
+
+	lastNonZeroFoundAt := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[lastNonZeroFoundAt] = nums[i]
+			lastNonZeroFoundAt++
+		}
+	}
+
+	for i := lastNonZeroFoundAt; i < len(nums); i++ {
+		nums[i] = 0
+	}
+}
