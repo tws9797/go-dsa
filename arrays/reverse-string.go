8bef8defe8b387013e511ce2fe88f0831ef47329
Tee Wen Seng <wensengtee@gmail.com>
2022-09-05 13:16:19 +0800

    reverse-string

---
+++ arrays/reverse-string.go
@@ -0,0 +1,11 @@
+package arrays
+
+// https://leetcode.com/problems/reverse-string/
+func reverseString(s []byte) {
+
+	if len(s) > 1 {
+		for low, high := 0, len(s)-1; low <= high; low, high = low+1, high-1 {
+			s[low], s[high] = s[high], s[low]
+		}
+	}
+}
