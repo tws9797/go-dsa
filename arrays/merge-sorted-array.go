1a3c8c23b9fa95f8dc6dadf24dfff0318e190853
Tee Wen Seng <wensengtee@gmail.com>
2022-09-05 14:25:53 +0800

    merge-sorted-array

---
+++ arrays/merge-sorted-array.go
@@ -0,0 +1,30 @@
+package arrays
+
+// https://leetcode.com/problems/merge-sorted-array/
+func merge(nums1 []int, m int, nums2 []int, n int) {
+
+	lenMerge := m + n
+
+	for i := lenMerge - 1; i >= 0; i-- {
+
+		// If n == 0, leave nums1 as it is
+		if n == 0 {
+			break
+		} else if m == 0 {
+
+			// If m == 0, copy the rest of nums2 to nums1
+			nums1[i] = nums2[n-1]
+			n--
+		} else {
+
+			// Put the greater number at the end of nums1
+			if nums1[m-1] > nums2[n-1] {
+				nums1[i] = nums1[m-1]
+				m--
+			} else {
+				nums1[i] = nums2[n-1]
+				n--
+			}
+		}
+	}
+}
