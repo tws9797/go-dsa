7f29c231a86e486c6c6deec1fa3a6cc55429795e
Tee Wen Seng <wensengtee@gmail.com>
2022-09-07 21:16:54 +0800

    insertion-sort

---
+++ sorting/insertion-sort.go
@@ -0,0 +1,14 @@
+package sorting
+
+func InsertionSort(arr []int) {
+
+	for i := 1; i < len(arr); i++ {
+
+		for j := i; j > 0; j-- {
+			if arr[j-1] > arr[j] {
+				arr[j-1], arr[j] = arr[j], arr[j-1]
+			}
+		}
+	}
+
+}
